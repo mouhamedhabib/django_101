7131b818585614dd809549e06d05615bc93eadab
habib <chaiebmohamedhabib1@gmail.com>
2024-08-27 20:08:59 +0100

    test

--- feed/urls.py
+++ feed/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from .views import HomePageView , postDetailView , AddPostView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = 'feed'
 
@@ -11,5 +12,6 @@ urlpatterns = [
      path ('post/', AddPostView.as_view(), name='post'),
 ]
 
+urlpatterns += staticfiles_urlpatterns()
 if settings.DEBUG :
      urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
--- my_frist_fsite/settings.py
+++ my_frist_fsite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,7 +86,9 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+DATABASES = {
+    "default" : dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
